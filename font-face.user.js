0245a47f95b78d20abae15e3f826170b04ab67e8
aenerv7 <aenerv7@gmail.com>
2019-07-03 18:00:19 +0800

    Update font-face A Add font-face UserScript version M Update font-face css

---
+++ font-face.user.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name          中文字体优化
+// @description	  根据平台自适应中文字体优化
+// @author        aenerv7
+// @run-at        document-start
+// @version       1.2
+// @namespace 	  https://greasyfork.org/users/4220
+// ==/UserScript==
+(function () {
+	var css = "@font-face { font - family: serif; src: local('PingFang SC'), local('Microsoft YaHei UI'); unicode - range: U + 0 - 10FFFF; } \n";
+	css = css.concat("@font-face { font - family: sans-serif; src: local('PingFang SC'), local('Microsoft YaHei UI'); unicode - range: U + 0 - 10FFFF; } \n");
+	css = css.concat("@font-face { font - family: -webkit-standard; src: local('PingFang SC'), local('Microsoft YaHei UI'); unicode - range: U + 0 - 10FFFF; } \n");
+	css = css.concat("@font-face { font - family: monospace; src: local('Sarasa Term SC'); unicode - range: U + 0 - 10FFFF; } \n");
+	css = css.concat("@font-face { font-family: 'Heiti SC'; src: local('PingFang SC'), local('Microsoft YaHei UI'); } \n");
+	css = css.concat("@font-face { font-family: 'Hiragino Sans GB'; src: local('PingFang SC'), local('Microsoft YaHei UI'); } \n");
+	css = css.concat("@font-face { font-family: 'Microsoft Yahei'; src: local('PingFang SC'), local('Microsoft YaHei UI'); } \n");
+	css = css.concat("@font-face { font-family: 'Microsoft YaHei'; src: local('PingFang SC'), local('Microsoft YaHei UI'); } \n");
+	css = css.concat("@font-face { font-family: 'WenQuanYi Micro Hei'; src: local('PingFang SC'), local('Microsoft YaHei UI'); } \n");
+	css = css.concat("@font-face { font-family: Simhei; src: local('PingFang SC'), local('Microsoft YaHei UI'); } \n");
+	css = css.concat("@font-face { font-family: Simsun; src: local('PingFang SC'), local('Microsoft YaHei UI'); } \n");
+	css = css.concat("@font-face { font-family: STHeiti; src: local('PingFang SC'), local('Microsoft YaHei UI'); } \n");
+	css = css.concat("@font-face { font-family: 黑体; src: local('PingFang SC'), local('Microsoft YaHei UI'); } \n");
+	css = css.concat("@font-face { font-family: '黑体'; src: local('PingFang SC'), local('Microsoft YaHei UI'); } \n");
+	css = css.concat("@font-face { font-family: 华文黑体; src: local('PingFang SC'), local('Microsoft YaHei UI'); } \n");
+	css = css.concat("@font-face { font-family: 宋体; src: local('PingFang SC'), local('Microsoft YaHei UI'); } \n");
+	css = css.concat("@font-face { font-family: '宋体'; src: local('PingFang SC'), local('Microsoft YaHei UI'); } \n");
+	css = css.concat("@font-face { font-family: 宋體; src: local('PingFang SC'), local('Microsoft YaHei UI'); } \n");
+	css = css.concat("@font-face { font-family: '宋體'; src: local('PingFang SC'), local('Microsoft YaHei UI'); } \n");
+	css = css.concat("@font-face { font-family: 'Lucida Console'; src: local('Sarasa Term SC'); }");
+	if (typeof GM_addStyle != "undefined") {
+		GM_addStyle(css);
+	} else if (typeof PRO_addStyle != "undefined") {
+		PRO_addStyle(css);
+	} else if (typeof addStyle != "undefined") {
+		addStyle(css);
+	} else {
+		var node = document.createElement("style");
+		node.type = "text/css";
+		node.appendChild(document.createTextNode(css));
+		var heads = document.getElementsByTagName("head");
+		if (heads.length > 0) {
+			heads[0].appendChild(node);
+		} else {
+			// no head yet, stick it whereever
+			document.documentElement.appendChild(node);
+		}
+	}
+})();
