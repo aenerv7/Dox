db5cc58242859a0543442c01141fe6b6d6836f70
aenerv7 <aenerv7@gmail.com>
2023-03-10 11:20:24 +0800

    Update stash js

--- Stash/external-ip-address-tile.js
+++ Stash/external-ip-address-tile.js
@@ -1,7 +1,16 @@
 $httpClient.get("http://ip-api.com/json/?lang=zh-CN", function (error, response, data) {
-    let parsed_data = JSON.parse(data);
-    let content_string = parsed_data.query + " (" + parsed_data.country + ")";
-    $done({
-        content: content_string,
-    })
+    if(error)
+    {
+        $done({
+            content: "获取失败",
+        })
+    }
+    else
+    {
+        let parsed_data = JSON.parse(data);
+        let content_string = parsed_data.query + "@" + parsed_data.country;
+        $done({
+            content: content_string,
+        })
+    }
 })
