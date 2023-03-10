65096e25490eb06ea99b00ef4650b4b44b5f9b12
aenerv7 <aenerv7@gmail.com>
2023-03-10 11:14:11 +0800

    Update stash js

--- Stash/external-ip-address-tile.js
+++ Stash/external-ip-address-tile.js
@@ -2,9 +2,6 @@ $httpClient.get("https://api.country.is/", function (error, response, data) {
     let parsed_data = JSON.parse(data);
     let content_string = parsed_data.ip + " (" + parsed_data.country + ")";
     $done({
-        title: "IP",
         content: content_string,
-        backgroundColor: "#131313",
-        icon: "network",
     })
 })
